b7ee4921d43bac9a3ff75203b1441424d4888066
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-03-05 18:29:12 +0530

    updated url

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://mern-booking-app-blond.vercel.app/",
+    origin: "https://mern-booking-app-blond.vercel.app",
   })
 );
 
